fe57218ccd6650db6cc9697189a827b2e5c6bd58
ttechgirl <abiodunayisat2@gmail.com>
2024-10-16 12:47:46 +0100

    changes

---
+++ StudentTutorial/Collection.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTutorial
+{
+    public static class Collection
+    {
+        public static void ArrayLists()
+        {
+
+            //Adding elements to ArrayList using Add() method
+            ArrayList arrayList1 = new ArrayList();
+           
+            arrayList1.Add(101);
+            arrayList1.Add("Shazy");
+            arrayList1.Add(" ");
+            arrayList1.Add(true);
+            arrayList1.Add(4.5);
+            arrayList1.Add(null);
+
+            //Adding Elements to ArrayList using object initializer syntax
+            var arrayList2 = new ArrayList
+            {
+                200,
+                "Adeola",
+                true,
+                true,
+                45.80F,
+                'B'
+
+            };
+            
+
+            //arrayList1.InsertRange(2, arrayList2);
+            //iterating through foreach loop
+            //foreach (var item in arrayList1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            // Console.WriteLine("=======================================");
+
+            ////iterating through for loop
+            //for (int i = 0; i < arrayList1.Count; i++)
+            //{
+            //    Console.WriteLine(arrayList1[i]);
+            //}
+
+            // Console.WriteLine("=======================================");
+            ////accessing elements using indexer
+            //int first = (int)arrayList1[0];
+            //Console.WriteLine(first);
+
+            // Console.WriteLine("=======================================");
+
+            ////using var
+            //var fourth = arrayList2[3];
+            //    Console.WriteLine(fourth);
+
+            // Console.WriteLine("=======================================");
+
+            //adding elements of arrayList1 to arrayList2 
+            //arrayList2.AddRange(arrayList1);
+            //foreach (var item in arrayList2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // Console.WriteLine("=======================================");
+            //remove range of elements from arraylist
+            //arrayList1.RemoveRange(2, 3);
+            //foreach (var item in arrayList1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("=======================================");
+            ////remove element at a specific index from arraylist collection 
+            //arrayList2.RemoveAt(1);
+            //foreach (var item in arrayList2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("=======================================");
+            //arrayList2.Remove(3);
+            //foreach (var item in arrayList2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Hashtable
+
+
+        }
+
+
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public char Gender { get; set; }
+            public string JobRole { get; set; }
+            public bool IsActive { get; set; }
+        }
+
+        public interface ICompanyStaffs
+        {
+            public IList<Employee> Employees { get; set;}
+        }   
+    }
+}
+    
---
+++ StudentTutorial/OOPs.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTutorial
+{
+    public class OOPs
+    {
+       //nested classes
+        public class PureCsharp
+        {
+            public void Generator()
+            {
+                Console.WriteLine("Enter password");
+                Console.WriteLine("=================");
+                Console.WriteLine("Password must be 6 characters long");
+                Console.WriteLine("=================");
+
+                var password = Console.ReadLine();
+
+                if (password != null && password.Length >= 6)
+                {
+                    var guid = Guid.NewGuid().ToString("N")[2..6];
+                    int age = 5;
+                    var generatedPassword = password + " " + guid; //string concatenation
+                    Console.WriteLine($"Suggested password is {generatedPassword}"); //string interpolation
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("=================");
+                    Console.WriteLine("Wrong password format");
+                    return;
+                }
+            }
+        }
+
+        //Inheritance , polymorphism and overriding
+        public class Zoo  //base
+        {
+            public virtual void Animal()
+            {
+                Console.WriteLine("This is the base class");
+            }
+        }
+
+
+        public class Zoom : Zoo
+        {
+            public override void Animal()
+            {
+                Console.WriteLine("This is the derived class");
+
+            }
+
+        }
+
+       // Abstraction
+        public interface ISchool
+        {
+            void Student();
+            public string BookShop { get; set; }
+
+        }
+
+        public class School : ISchool
+        {
+            public string BookShop { get => BookShop; set => throw new NotImplementedException(); } //property
+
+            public void Student()
+            {
+                var age = 40;
+                var name = "Shola";
+
+                Console.WriteLine($"Student name is {name}");
+
+                //data types
+                int blue = 100;
+                string firstName = "Victoria";
+                firstName = "Ajoke"; //reassignment
+                char grade = 'A';
+                Console.WriteLine($"Hello, {firstName} got {grade} in her english exam!"); //string interpolation
+                Console.WriteLine($"{firstName} is got {blue} in her english exam!"); //string interpolation
+                return;
+            }
+
+            public void Football()
+            {
+                //data types
+
+                return;
+            }
+
+        }
+    }
+}
